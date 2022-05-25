cd4d5819969434bac8aa8656468f4f1fea37844e
Brice Videau <bvideau@anl.gov>
2022-05-25 17:26:00 -0500

    more robust test.

--- test/test_ffi_value.rb
+++ test/test_ffi_value.rb
@@ -4,8 +4,11 @@ require 'ffi-value'
 
 module SmallPthread
   extend FFI::Library
-  ffi_lib "pthread"
-
+  begin
+    ffi_lib "pthread"
+  rescue LoadError
+    ffi_lib "libpthread.so.0"
+  end
   callback :start_routine, [:value], :pointer
   attach_function :pthread_create, [:pointer, :pointer, :start_routine, :value], :int
   attach_function :pthread_join, [:ulong, :pointer], :int, blocking: true
@@ -24,6 +27,19 @@ class TestFFIValue < Minitest::Test
     res = SmallPthread.pthread_join(ptid.read_ulong, pret)
     assert_equal(0, res)
     assert(pret.read_pointer.null?)
+  end
+
+  def test_memory
+    vals = [ :foo, 15, nil ]
+    mem = FFI::MemoryPointer.new(:value, vals.size)
+    mem.put_value(0, :foo)
+    assert_equal(:foo, mem.read_value)
+    mem.write_value(:bar)
+    assert_equal(:bar, mem.get_value(0))
+    mem.put_array_of_value(0, vals)
+    assert_equal(vals, mem.read_array_of_value(vals.size))
+    mem.write_array_of_value(vals.reverse)
+    assert_equal(vals.reverse, mem.get_array_of_value(0, vals.size))
   end
   
 end
