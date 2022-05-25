a05e48878c1036435f060a65f86d255b61fc2cde
Brice Videau <bvideau@anl.gov>
2022-05-25 17:25:44 -0500

    Added (de)serializers for ruby values.

--- lib/ffi-value.rb
+++ lib/ffi-value.rb
@@ -23,4 +23,58 @@ module FFI
   end
 
   typedef m, :value
+
+  class AbstractMemory
+    if FFI::Type::POINTER.size == 8
+      def put_value(offset, value)
+        put_uint64(offset, value.object_id)
+      end
+      def write_value(value)
+        write_uint64(value.object_id)
+      end
+      def put_array_of_value(offset, ary)
+        put_array_of_uint64(offset, ary.collect(&:object_id))
+      end
+      def write_array_of_value(ary)
+        write_array_of_uint64(ary.collect(&:object_id))
+      end
+      def get_value(offset)
+        ObjectSpace._id2ref(get_uint64(offset))
+      end
+      def read_value
+        ObjectSpace._id2ref(read_uint64)
+      end
+      def get_array_of_value(offset, length)
+        get_array_of_uint64(offset, length).collect { |v| ObjectSpace._id2ref(v) }
+      end
+      def read_array_of_value(length)
+        read_array_of_uint64(length).collect { |v| ObjectSpace._id2ref(v) }
+      end
+    else
+      def put_value(offset, value)
+        put_uint32(offset, value.object_id)
+      end
+      def write_value(value)
+        write_uint32(value.object_id)
+      end
+      def put_array_of_value(offset, ary)
+        put_array_of_uint32(offset, ary.collect(&:object_id))
+      end
+      def write_array_of_value(ary)
+        write_array_of_uint32(ary.collect(&:object_id))
+      end
+      def get_value(offset)
+        ObjectSpace._id2ref(get_uint32(offset))
+      end
+      def read_value
+        ObjectSpace._id2ref(read_uint32)
+      end
+      def get_array_of_value(offset, length)
+        get_array_of_uint32(offset, length).collect { |v| ObjectSpace._id2ref(v) }
+      end
+      def read_array_of_value(length)
+        read_array_of_uint32(length).collect { |v| ObjectSpace._id2ref(v) }
+      end
+    end
+  end
 end
