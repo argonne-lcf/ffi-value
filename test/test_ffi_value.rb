5702ddf4f57631060a035a9959424886cbd82a7f
Brice Videau <bvideau@anl.gov>
2024-07-08 17:45:15 -0500

    Add refcounting of objects for callback purposes.

--- test/test_ffi_value.rb
+++ test/test_ffi_value.rb
@@ -12,6 +12,7 @@ module SmallPthread
   callback :start_routine, [:value], :pointer
   attach_function :pthread_create, [:pointer, :pointer, :start_routine, :value], :int
   attach_function :pthread_join, [:ulong, :pointer], :int, blocking: true
+  attach_function :pthread_create2, :pthread_create, [:pointer, :pointer, :start_routine, :pointer], :int
 end
 
 class TestFFIValue < Minitest::Test
@@ -29,6 +30,38 @@ class TestFFIValue < Minitest::Test
     assert_equal(0, res)
     assert(success)
     assert(pret.read_pointer.null?)
+  end
+
+  def test_volatile_value
+    success = false
+    l = lambda { |v| success = (v == { foo: 1, bar: "baz" }); FFI.dec_ref(v); nil }
+    val = { foo: 1, bar: "baz" }
+    FFI.inc_ref(val)
+    val = val.object_id
+    GC.start
+
+    ptid = FFI::MemoryPointer.new(:ulong)
+    res = SmallPthread.pthread_create2(ptid, nil, l, FFI::Pointer.new(val))
+    assert_equal(0, res)
+    pret = FFI::MemoryPointer.new(:pointer)
+    res = SmallPthread.pthread_join(ptid.read_ulong, pret)
+    assert_equal(0, res)
+    assert(success)
+    assert(pret.read_pointer.null?)
+    GC.start
+    assert_raises(RangeError, "\"#{val}\" is recycled object") { ObjectSpace._id2ref(val) }
+  end
+
+  def test_ref_count
+    obj = "foo"
+    assert_raises(ArgumentError, "Object: #{obj.object_id} was not previously referenced!") { FFI.dec_ref(obj) }
+    assert_equal(1, FFI.inc_ref(obj))
+    assert_equal(2, FFI.inc_ref(obj))
+    assert_equal(1, FFI.dec_ref(obj))
+    assert_equal(2, FFI.inc_ref(obj))
+    assert_equal(1, FFI.dec_ref(obj))
+    assert_equal(0, FFI.dec_ref(obj))
+    assert_raises(ArgumentError, "Object: #{obj.object_id} was not previously referenced!") { FFI.dec_ref(obj) }
   end
 
   def test_memory
